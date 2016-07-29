5b705ba513fb33bae3b673e8409730f1216c5943
penguinwatcher <penguinwatcher@gmail.com>
2016-07-30 01:14:33 +0900

    modifies flake8 error.

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ if __name__ == '__main__':
     sys.path.append('./magnet')
     sys.path.append('./test')
     setup(
-        name = 'magnet',
-        version = '0.1.0',
-        packages = find_packages(),
-        install_requires = [
+        name='magnet',
+        version='0.1.0',
+        packages=find_packages(),
+        install_requires=[
             'tornado',
         ],
-        entry_points = """
+        entry_points="""
         [console_scripts]
         magnet=magnet.boot:boot
         magnet-cli=magnet.cli:invoke_cli
         """
     )
 
+# EOF
